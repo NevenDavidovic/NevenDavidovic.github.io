3c89237ad46dc7862b403c564140ac38c25f5c9c
NevenDavidovic <nenoronnie@gmail.com>
2023-03-27 17:23:53 +0200

    inserting JS

---
+++ js/cv.js
@@ -0,0 +1,19 @@
+    // Get the container element
+    const container = document.querySelector('.container');
+  
+    // Set the initial opacity and position
+    container.style.opacity = 0;
+    container.style.transform = 'translateY(50px)';
+    
+    // Animate the element when the page loads
+    window.onload = () => {
+      container.style.opacity = 1;
+      container.style.transform = 'translateY(0)';
+      container.style.transition = 'opacity 1s, transform 1s';
+    };
+
+    function downloadCV() {
+        if (confirm("Are you sure you want to download CV from this awesome guy?")) {
+          window.location.href = "documents/cv.pdf";
+        }
+      }
