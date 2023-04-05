a78b9e5eca7b80d4781a1cbfde4649081f836766
NevenDavidovic <nenoronnie@gmmail.com>
2023-04-05 23:41:48 +0200

    js formating

--- js/cv.js
+++ js/cv.js
@@ -1,21 +1,21 @@
-    // Get the container element
-    const container = document.querySelector('.container');
-    
-  
-    // Set the initial opacity and position
-    container.style.opacity = 0;
-    container.style.transform = 'translateY(50px)';
-    
-    // Animate the element when the page loads
-    window.onload = () => {
-      container.style.opacity = 1;
-      container.style.transform = 'translateY(0)';
-      container.style.transition = 'opacity 1s, transform 1s';
-    };
+"use strict";
 
-    function downloadCV() {
-        if (confirm("Are you sure you want to download CV of this awesome guy?")) {
-          window.location.href = "documents/cv.pdf";
-        }
-      }
+// Get the container element
+const container = document.querySelector(".container");
 
+// Set the initial opacity and position
+container.style.opacity = 0;
+container.style.transform = "translateY(50px)";
+
+// Animate the element when the page loads
+window.onload = () => {
+  container.style.opacity = 1;
+  container.style.transform = "translateY(0)";
+  container.style.transition = "opacity 1s, transform 1s";
+};
+
+function downloadCV() {
+  if (confirm("Are you sure you want to download CV of this awesome guy?")) {
+    window.location.href = "documents/cv.pdf";
+  }
+}
--- js/osobnastranica.js
+++ js/osobnastranica.js
@@ -1,26 +1,28 @@
+"use strict";
+
 // Get the middle-container and chess elements
-const mContainer = document.querySelector('.middle-container');
-const container = document.querySelector('.right-container');
-const tcontainer = document.querySelector('.left-container');
+const mContainer = document.querySelector(".middle-container");
+const container = document.querySelector(".right-container");
+const tcontainer = document.querySelector(".left-container");
 // Set the initial opacity and position
 mContainer.style.opacity = 0;
-mContainer.style.transform = 'translateY(50px)';
+mContainer.style.transform = "translateY(50px)";
 container.style.opacity = 0;
-container.style.transform = 'translateY(50px)';
+container.style.transform = "translateY(50px)";
 tcontainer.style.opacity = 0;
-tcontainer.style.transform = 'translateY(50px)';
+tcontainer.style.transform = "translateY(50px)";
 
 // Animate the elements when the page loads
 window.onload = () => {
   mContainer.style.opacity = 1;
-  mContainer.style.transform = 'translateY(0)';
-  mContainer.style.transition = 'opacity 1s, transform 1s';
+  mContainer.style.transform = "translateY(0)";
+  mContainer.style.transition = "opacity 1s, transform 1s";
 
   container.style.opacity = 1;
-  container.style.transform = 'translateY(0)';
-  container.style.transition = 'opacity 1s, transform 1s';
-  
+  container.style.transform = "translateY(0)";
+  container.style.transition = "opacity 1s, transform 1s";
+
   tcontainer.style.opacity = 1;
-  tcontainer.style.transform = 'translateY(0)';
-  tcontainer.style.transition = 'opacity 1s, transform 1s';
+  tcontainer.style.transform = "translateY(0)";
+  tcontainer.style.transition = "opacity 1s, transform 1s";
 };
